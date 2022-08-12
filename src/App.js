ae3762589f0a79101b9c18a01fced35989f53dd7
Miguel Nicolas Gimenez Aviles <nickgyta@gmail.com>
2022-08-12 10:57:12 -0300

    prueba 1

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nico Gimenez primera App de ReactJs.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nickgyta/ReactJsProyect.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mi Github aquí..
         </a>
       </header>
     </div>
